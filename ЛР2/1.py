79e948a52b771b01d3bab7393731b09379517fa5
maybeiiira <iiiradalimova@gmail.com>
2024-10-27 20:59:08 +0300

    Add files via upload

---
+++ ЛР2/1.py
@@ -0,0 +1,15 @@
+salary = 5000  # Ежемесячная зарплата
+spend = 6000  # Траты за первый месяц
+months = 10  # Количество месяцев, которое планируется протянуть без долгов
+increase = 0.03  # Ежемесячный рост цен
+
+required_savings = 0
+
+for _ in range(months):
+    required_savings += spend - salary
+    spend *= (1 + increase)
+
+required_savings = round(required_savings)
+# TODO Рассчитайте подушку безопасности, чтобы протянуть 10 месяцев без долгов
+
+print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", required_savings)
